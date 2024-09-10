37538006dd4b040a80f4e40f18dbbbe2adcfe917
FranciscoSole <franciscosoleescande@gmail.com>
2024-09-10 20:44:42 -0300

    Minimal typo change

--- SZMisc/serverMessages.lua
+++ SZMisc/serverMessages.lua
@@ -1,13 +1,13 @@
- function _serverErrorMsg(message)
+function _serverErrorMsg(message)
     local errorMsg = "#DD0606[Error]#FFFFFF"
-    local msg = errorMsg.. " "..message
+    local msg = errorMsg.." "..message
 
     outputChatBox(msg, thePlayer, 255, 255, 255, true)
 end
 
 function _serverInformationMsg(message)
     local informationMsg = "#009999[Information]#FFFFFF"
-    local msg = informationMsg.. " "..message
+    local msg = informationMsg.." "..message
 
     outputChatBox(msg, thePlayer, 255, 255, 255, true)
 end
